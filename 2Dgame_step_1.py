1af6e9b81f9192a7f2380d9dfcab2042601dd2a1
peachspeed_com <131349605+peachspeed@users.noreply.github.com>
2024-10-21 05:58:32 +0900

    구현해야 할 것 함수로 적기

--- 2Dgame_step_1.py
+++ 2Dgame_step_1.py
@@ -1,1 +1,20 @@
-print("2D게임프로그래밍")
+from pico2d import *
+import random
+
+def menu():
+    pass
+
+def rhythm_game():
+    pass
+
+def RPG_confrontation():
+    pass
+
+def costume():
+    pass
+
+def makeup():
+    pass
+
+def random_ability():
+    pass
